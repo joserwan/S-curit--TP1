5e0d8a8e8746643898cd0608faa1dd5628ce3bc4
Erwan Jossic <joserwan@gmail.com>
2014-03-01 12:01:14 -0500

    Last revision

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Pattern;
 
 // http://archive.apache.org/dist/commons/codec/binaries/
 import org.apache.commons.codec.digest.*;
@@ -21,43 +23,73 @@ import org.apache.commons.codec.digest.*;
  *
  */
 public class Main {
-	static char[] chars = new char[]{'e','s','a','i','t','n','b','c','d','f','g','h','j','k','l','m','o','p','q','r','u','v','w','x','y','z','!','@','#','$','%','&','*'};
+	static char[] chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%&*".toCharArray();
 	static int maxPasswordLength = 8;
 	static char[] currentTestedPassword = new char[maxPasswordLength];
 	static String currentTestedPasswordString;
 	static int passwordTestedCount = 0;
+	static long[] startPerPasswordSize = new long[maxPasswordLength+1]; // On va inscrire le timestamp en milisecondes à l'index 1 pour les mots de passe de 1 caractère, par exemple. Pour fins de mesure de durée. 
 	
 	static List<String> hashes;
 	static List<String> clearPasswords;
 	static List<Thread> threads;
 	static int procs = Runtime.getRuntime().availableProcessors();
-	static ExecutorService threadExecutor = Executors.newFixedThreadPool( procs );
+	static ExecutorService threadExecutor = Executors.newFixedThreadPool( procs * 2 );
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Starting on " + procs + " threads\n");
-		String md5HashesPath = "/home/ejossic/Dropbox/documents/UQAR/Sécurité/hashed_passwords.txt";
-		String dictionnaryPath = "/home/ejossic/Dropbox/documents/UQAR/Sécurité/mots-8-et-moins.txt";
+		validateArguments(args);
+		
+		System.out.print("Utilisation de " + procs + " threads\n");
+		
+		String md5HashesPath = args[0];
+		String dictionnaryPath = args[1];
 
-		hashes = fileLinesToArray(md5HashesPath);
-		clearPasswords = fileLinesToArray(dictionnaryPath);
+		hashes = fileLinesToArray(md5HashesPath, true);
+		clearPasswords = fileLinesToArray(dictionnaryPath, false);
 
 		dictionnaryAttack(hashes, clearPasswords);
+		bruteForceAttack(hashes); // Les hashs trouvés ont été enlevés
+	}
+
+	private static void validateArguments(String[] args) {
+		if(args.length != 2){
+			displayUsage();
+			System.exit(1);
+		}
+		
+		for(int i=0; i<args.length; i++){
+			File f = new File(args[i]);
+			if(!f.exists()){
+				System.out.printf("%s n'existe pas !\n", f.toPath().toString());
+				displayUsage();
+				System.exit(2);
+			}
+		}
+	}
+
+	private static void displayUsage() {
+		System.out.println("Usage:\njava -jar Main.jar path/to/md5_list path/to/password_directory");		
+	}
+
+	/*
+	 * Attaque de type "Brute-force" multi-threads.
+	 * Chaque Thread est chargé de tester toutes les mots de passe possibles
+	 * de taille T et commençant par une lettre L 
+	 */
+	private static void bruteForceAttack(List<String> hashes) {
 		System.out.print("\n\nIl reste " + Integer.toString(hashes.size()) + " MD5 à trouver ...\n\n");
 		for(int i=0; i<hashes.size(); i++){
 			System.out.print(hashes.get(i) + "\n");
 		}
-		bruteForceAttack(hashes);
-		System.out.print("==============[ Terminé ]===============");
-	}
-
-	private static void bruteForceAttack(List<String> hashes) {
+		
 		for(int i=1; i <= maxPasswordLength; i++){
 			for(int j=0; j < chars.length; j++){
 				PasswordBruteForcer bruteForcer = new PasswordBruteForcer(hashes, i, chars, j);
+				
+				// Pour moins de 5 caractères, ça ne faut pas la peine de prendre le temps d'initialiser des threads
 				if(i < 5){
 					bruteForcer.run();
 				}
@@ -68,26 +100,6 @@ public class Main {
 		}
 		threadExecutor.shutdown();
 		return;
-	}
-
-	private static void testEachCharNextTo(int passwordCharPosition) {
-		if(passwordCharPosition >= maxPasswordLength){ return; }
-		
-		for(int i=0;i<chars.length;i++){
-			currentTestedPassword[passwordCharPosition] = chars[i];
-					
-			currentTestedPasswordString = new String(currentTestedPassword,0,passwordCharPosition+1);
-			//System.out.print("\nHashing '" + password + "' - " + Integer.toString(password.length()) + ") [" + Integer.toString(passwordCharPosition) + "," + Integer.toString(i) + "]");
-			if(passwordTestedCount % 1000000 ==0) System.out.print("\r" +Integer.toString(passwordTestedCount) + " passwords testés (" + currentTestedPasswordString  + ")");
-			String digest = DigestUtils.md5Hex(currentTestedPasswordString);
-			int index = hashes.indexOf(digest);
-			if(index >= 0){
-				System.out.print("\n" + hashes.get(index) + " concorde avec " + currentTestedPasswordString + "\n");
-				hashes.remove(index);
-			}
-			passwordTestedCount++;
-			testEachCharNextTo(passwordCharPosition+1);
-		}
 	}
 	/*
 	 * Hashage des mots de passe en clair 
@@ -114,20 +126,27 @@ public class Main {
 	/*
 	 * Génère une liste de Chaine de caractères 
 	 * après lecture du fichier passé en paramètre
+	 * 
+	 * http://www.java-samples.com/showtutorial.php?tutorialid=1299
 	 */
-	public static List<String> fileLinesToArray(String file_path){
+	public static List<String> fileLinesToArray(String file_path, boolean isMD5){
 		List<String> listOfString = new ArrayList<String>();
 		FileInputStream fstream;
+		Pattern md5Pattern = Pattern.compile("^[a-z0-9]{32}$");
 		try {
 			fstream = new FileInputStream(file_path);
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		  	String strLine;
 		  	while ((strLine = br.readLine()) != null)   {
-		  		// Print the content on the console
-		  		listOfString.add(strLine);
+		  		if(!isMD5 || md5Pattern.matcher(strLine).matches()){
+		  			listOfString.add(strLine);
+		  		}
+		  		else{
+		  			System.err.print("Erreur avec " + strLine);
+		  		}
 		  	}
-			  //Close the input stream
+		  	//Close the input stream
 			in.close();
 		}  catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/PasswordBruteForcer.java
+++ src/PasswordBruteForcer.java
@@ -1,5 +1,6 @@
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.twmacinta.util.*;
 
@@ -21,28 +22,44 @@ public class PasswordBruteForcer implements Runnable {
 	int passwordTestedCount;
 	Long passwordTotalCount;
 	List<String> hashes;
+	MD5 md5;
 	
 	public PasswordBruteForcer(List<String> hashes, int passwordLength, char[] chars, int firstCharIndex){
 		this.chars = chars;
 		this.hashes = hashes;
 		this.firstCharIndex = firstCharIndex;
 		this.passwordLength = passwordLength;
+		this.md5 = new MD5();
 		currentTestedPassword = new char[this.passwordLength];
+		
 		for(int i =0; i<this.currentTestedPassword.length;i++){
+			// On démarre avec la première lettre voulue firstCharIndex,
+			// puis on remplis le mot de passe de départ de 'a'
 			if(i == 0) currentTestedPassword[i] = this.chars[firstCharIndex];
 			else currentTestedPassword[i] = this.chars[0];
 		}
 		passwordTotalCount = (long) (Math.pow(new Double(this.chars.length), new Double(passwordLength-1)));
 	}
+	
+	
 	public void run() {
 		System.out.print("\n Thread #" + Thread.currentThread().getId() + "\t Testing " + chars[this.firstCharIndex] + "\\w{"+ Integer.toString(passwordLength-1) + "} *******");
-		try{
-			run(this.passwordLength == 1 ? 0 : 1,0);
+
+		// Démarrage du compteur de temps
+		if(this.firstCharIndex == 0){ 
+			Main.startPerPasswordSize[this.passwordLength] = System.currentTimeMillis(); 
 		}
-		catch(Exception e){
-			System.err.print("run(" + (this.passwordLength == 1 ? 0 : 1) + "," + 0 + ") -> " + e.getMessage());
+		
+		long startedAt = System.currentTimeMillis();
+		run((this.passwordLength == 1 ? 0 : 1), 0);
+		System.out.print("\r" + currentTestedPasswordString  + " - " + passwordTestedCount/(float)passwordTotalCount*100 + "% - " + Long.toString(passwordTestedCount) + " en " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startedAt) + "s.");
+		
+		
+		if(this.firstCharIndex == this.chars[this.chars.length - 1]){
+			// Note : Il se peut que le dernier caractère à tester ne soit pas le dernier à terminer
+			// Cependant il ne devrait pas en être très loin, donc on tolèrera cette amrge d'erreur ici !
+			System.out.print("\nMots de passe de " + this.passwordLength + " caractères terminés en " + TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - Main.startPerPasswordSize[this.passwordLength]) + "m.\n");
 		}
-		System.out.print("\r" + this.passwordLength + "\t" + passwordTestedCount/(float)passwordTotalCount*100 + "% - " + Long.toString(passwordTestedCount) + " passwords testés (" + currentTestedPasswordString  + ")");
 	}
 	
 	private void run(int position, int charPosition) {
@@ -51,14 +68,18 @@ public class PasswordBruteForcer implements Runnable {
 			currentTestedPasswordString = new String(currentTestedPassword);
 			//System.out.print("\nHashing '" + password + "' - " + Integer.toString(password.length()) + ") [" + Integer.toString(passwordCharPosition) + "," + Integer.toString(i) + "]");
 			//String digest = DigestUtils.md5Hex(currentTestedPasswordString);
-			MD5 md5 = new MD5();
 		    try {
+		    	md5.Init();
 				md5.Update(currentTestedPasswordString, null);
 			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		    String digest = md5.asHex();
+		    
+		    if(passwordTestedCount % 1000000 == 0){
+		    	System.out.print("\r" + currentTestedPasswordString  + " - " + passwordTestedCount/(float)passwordTotalCount*100 + "% - " + Long.toString(passwordTestedCount));
+			}
+			
 			int index = hashes.indexOf(digest);
 			if(index >= 0){
 				System.out.print("\n" + hashes.get(index) + " concorde avec " + currentTestedPasswordString + "\n");
@@ -66,10 +87,6 @@ public class PasswordBruteForcer implements Runnable {
 			}
 
 			passwordTestedCount++;
-			
-			if(passwordTestedCount%5000000 == 0){
-				System.out.print("\r Thread #" + Thread.currentThread().getId() + " - " + this.passwordLength + "\t" + passwordTestedCount/(float)passwordTotalCount*100 + "% - " + Long.toString(passwordTestedCount) + " passwords testés (" + currentTestedPasswordString  + ")[" + position + "," + j + "]");
-			}
 			if(position < this.passwordLength-1) run(position+1, charPosition);
 		}
 	}
